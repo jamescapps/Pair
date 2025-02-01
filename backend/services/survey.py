1f9f7b84ef67b4fdee2980bc0f1843a54b650230
James Capps <jcapps@protonmail.com>
2025-01-31 21:58:29 -0600

    Added additional services and user logic.

---
+++ backend/services/survey.py
@@ -0,0 +1,15 @@
+class SurveyService(object):
+    def __init__(self):
+        pass
+
+    def get_survey(self):
+        pass
+
+    def answer_survey(self):
+        pass
+
+    def edit_survey_answers(self):
+        pass
+
+    def submit_survey(self):
+        pass
