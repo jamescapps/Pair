dedaf0403ed8e0f835b6ad2750d5f793b28b4f34
James Capps <jcapps@protonmail.com>
2025-02-15 17:47:52 -0600

    Framed in auth endpoints and formatted routes.

--- backend/app/message.py
+++ backend/app/message.py
@@ -7,15 +7,18 @@ router = APIRouter(prefix="/v1/message", tags=["message"])
 
 # TODO token authentication, svc dependency, response model
 
+
 @router.get("/number/unread")
 def get_num_of_unread_messages(user_id: int, svc: MessageService):
     return svc.get_num_of_unread_messages(user_id)
+
 
 @router.get("/")
 def get_messages(user_id: int, svc: MessageService):
     return svc.get_messages
 
-#TODO send to username instead of ID maybe
+
+# TODO send to username instead of ID maybe
 @router.post("/send")
-def send_message(from_user_id: int, to_user_id: int, svc: MessageService)
+def send_message(from_user_id: int, to_user_id: int, svc: MessageService):
     svc.send_message(from_user_id, to_user_id)
--- backend/app/user.py
+++ backend/app/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+
 from services.user import UserService
 from schema.profile import ProfileSchema
 
@@ -6,22 +7,27 @@ router = APIRouter(prefix="/v1/user", tags=["user"])
 
 # TODO token authentication, svc dependency, response model
 
+
 @router.get("/account")
 def get_user(user_id: int, svc: UserService):
     return svc.get_user(user_id)
+
 
 @router.post("/account")
 def add_user(profile_data: ProfileSchema, svc: UserService):
     svc.create_user(profile_data)
 
+
 @router.put("/account")
 def edit_user(user_id: int, profile_data: ProfileSchema, svc: UserService):
     return svc.edit_user(user_id, profile_data)
+
 
 @router.delete("/account")
 def delete_user(user_id: int, svc: UserService):
     svc.delete_user(user_id)
 
+
 @router.put("/account/deactivate")
-def deactivate_account(user_id: int, svc: UserService)
+def deactivate_account(user_id: int, svc: UserService):
     svc.deactivate_account(user_id)
