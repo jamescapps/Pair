8c6319888595e35eb992e106e73b98e4f23789e0
James Capps <jcapps@protonmail.com>
2025-02-15 17:40:01 -0600

    Added message routes.

--- backend/services/message.py
+++ backend/services/message.py
@@ -1,12 +1,15 @@
+from sqlalchemy.orm import Session
+
+
 class MessageService(object):
-    def __init__(self):
+    def __init__(self, session: Session):
+        self.sesssion = session
+
+    def get_num_of_unread_messages(self, user_id: int):
         pass
 
-    def get_num_of_messages(self):
+    def get_messages(self, user_id: int):
         pass
 
-    def get_messages(self):
-        pass
-
-    def send_message(self):
+    def send_message(self, from_user_id: int, to_user_id: int):
         pass
